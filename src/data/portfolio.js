44b8bf71b42357db4f535c23cb6469bd6aa82573
ARYAN MIDDLAGAJANI <aryanmiddlagajani@ARYANs-MacBook-Pro.local>
2020-06-02 21:32:13 +0530

    Add precompiled assets for Heroku

---
+++ src/data/portfolio.js
@@ -0,0 +1,65 @@
+const portfolioData = { 
+	"imageUrl": 'http://localhost:3000/',
+	"logo": "images/app-logo.svg",
+	"logoWhite": "images/app-logo-white.svg",
+	"name": 'Aryan D Middlagajani',
+	"role": 'Front-end Developer',
+	"intro": "I love to build and develop web applications using React,Html,Css,Javascript Frameworks and Intrested in Remote Jobs.",
+	"projects": [
+		{
+			"id": 0,
+			"title": "Mental Health Analysis Chatbot using Reactjs and Displaying Live Covid19 Tracker",
+			"imageUrl": "images/Mental health analysis and Covid19 tracker.jpeg",
+			"link": "https://polar-gorge-02337.herokuapp.com/Home",
+			"codeLink": "https://github.com/GuardianAngel008/Capstone",
+			"description": "Its helps people struggling with depression,anxiety,stress and gives advice and also this page shows the current Covid19 graph.",
+			"technology": ["HTML/CSS", "JavaScript", "React", ],
+		},	
+		
+										
+		],
+	"skills": [
+		{
+			"name": "HTML/CSS",
+			"percentage": 80
+		},
+		{
+			"name": "Javascript",
+			"percentage": 70
+		},	
+		{
+			"name": "ReactJS",
+			"percentage": 70
+		},							
+		{
+			"name": "C/C++",
+			"percentage": 80
+		},
+
+		{
+			"name": "Python",
+			"percentage": 70
+		},
+		{
+			"name": "Java",
+			"percentage": 80
+		},						
+	],
+	"workExp": [
+		{
+			"title": "My Projects",
+			"date" : 'June 2017- June 2019',
+			"description": "1)Worked on a Projects like Web Application for Film making and Social Networking 2)Worked on an Movie Reccomendation System Using Machine Learning 3)Worked on an Android Mobile Application  Project on Object Identifier using Java and XML(TENSORFLOW) .",
+			"company": 'My Overall Projects.'
+		},
+		{
+			"title": "Internship in Data Analytics",
+			"date" : 'May 2019 -  July 2019',
+			"company": 'RiteScribe MedInfo Pvt Ltd',
+			"description": " The project was meant to analyse the data and also predict the epidemics based on historical medical data."
+		},				
+	]	
+ 
+}
+
+export default portfolioData;
